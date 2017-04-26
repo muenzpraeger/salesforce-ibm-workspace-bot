798f426b83dafcddeb21a531c1d3cf2000c63f7e
René Winkelmeyer <mail@winkelmeyer.com>
2017-04-26 11:44:42 +0200

    Changed Alexa Space ID

--- node/alexaHandlers.js
+++ node/alexaHandlers.js
@@ -8,7 +8,7 @@ let request = require('request'),
 
 exports.PostWorkspace = (slots, session, response) => {
         salesforce.getTopOpportunities(3).then(opportunities => {
-            wwMessaging.sendMessage('', '59006743e4b0180503ee7087', '', wwFormatting.formatTopOpportunities(opportunities));
+            wwMessaging.sendMessage('', '59006be8e4b0180503ee7225', '', wwFormatting.formatTopOpportunities(opportunities));
         }); // TODO: We're using a fixed spaceId here - but you can built your own query for available spaces ;-)
     response.say("Ok, I posted the top opportunities");
 };
